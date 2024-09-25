b3a22af59b9222f66ff503912ef56b832af098c1
SVR Kalluru <raghuk.vit@gmail.com>
2024-09-25 06:33:52 +0530

    Ec2 Module

---
+++ run.sh
@@ -0,0 +1,17 @@
+if [ -z "${REDIS_HOST}" ]; then
+  echo Input REDIS_HOST is missing
+  exit 1
+fi
+
+if [ -z "${CATALOGUE_HOST}" ]; then
+  echo Input CATALOGUE_HOST is missing
+  exit 1
+fi
+
+if [ -z "${CATALOGUE_PORT}" ]; then
+  echo Input CATALOGUE_PORT is missing
+  exit 1
+fi
+
+
+/bin/node /app/server.js
